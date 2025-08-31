1c85f43096e84d7dc2487e3bfe98b3c23f6be9a9
Shivam Kumar <119954041+shivaarajput@users.noreply.github.com>
2025-08-31 21:46:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ app.add_middleware(
 )
 
 # Create a cloudscraper session
-scraper = cloudscraper.create_scraper()
+scraper = cloudscraper.create_scraper(browser={
+    'browser': 'chrome',
+    'platform': 'windows',
+    'mobile': False
+})
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
